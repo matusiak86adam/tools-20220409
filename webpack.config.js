62f7270e2d8ca0711a708f4d35a22bdcfc56ff82
matusiak86adam <matusiak86.adam@gmail.com>
2022-04-09 10:58:53 +0200

    zadanie3

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -6,4 +7,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+    template: './src/index.html',
+  }),
+],
 };
