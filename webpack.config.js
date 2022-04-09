5c0a0236c155ba47bcbda72610be8db156168906
matusiak86adam <matusiak86.adam@gmail.com>
2022-04-09 12:13:52 +0200

    zadanie6

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: ['./src/main.js', './src/style.css'],
@@ -11,7 +12,7 @@ module.exports = {
     rules: [
       {
         test: /.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
@@ -19,5 +20,6 @@ module.exports = {
     new HTMLWebpackPlugin({
     template: './src/index.html',
   }),
+  new MiniCssExtractPlugin(),
 ],
 };
