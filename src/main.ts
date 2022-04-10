e07ef538faa1e10c0805b61a55aef69c84bba271
matusiak86adam <matusiak86.adam@gmail.com>
2022-04-10 11:21:40 +0200

    TSconstFunction

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+const apiUrl = 'https://jsonplaceholder.typicode.com';
+
+const postsUrl: string  = apiUrl + '/posts';
+const commentsUrl: string = `${apiUrl}/comments`;
+const usersUrl: string = `${apiUrl}/users`;
+
+async function getApiResponse(url: string): Promise<any> {
+    const postsRequest: Promise<Response> = fetch(url);
+    const response: Response = await postsRequest;
+    const json: any = await response.json();
+    return json;
+  }
+  
